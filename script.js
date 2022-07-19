2303371546740999a213a04cc8522798bd7a23e3
Raul Cesar <raulcesar.rcf@gmail.com>
2022-07-19 19:15:40 -0300

    Enviando os exercicios da aula 007

---
+++ script.js
@@ -0,0 +1,70 @@
+// console.log(prompt("Qual seu nome?"))
+
+// console.log("Ola mundo")
+
+
+// const nome = "Raul Cesar"
+
+// console.log(nome)
+
+// let idade = 20
+
+// idade = 20.1
+
+// console.log(nome, idade)
+
+// console.log("Ola! meu nome e",nome, "e eu tenho", idade,"anos.")
+
+// console.log("idade", idade)
+
+// let temPet = false
+
+// const nomePet = "Lilica"
+
+//console.log(nomePet)
+// console.log(typeof temPet)
+// console.log(typeof nomePet)
+// console.log(typeof idade)
+
+// console.log(prompt("Qual o seu nome?"))
+//let idade = (Numberprompt("Qual a sua idade?"))
+
+
+// const nome = prompt("Qual o seu nome?")
+// let idade = prompt("Qual a sua idade?")
+// const idadeNumber = Number(idade)
+// const idadeString = idade.toString()
+
+
+// console.log(typeof idade)
+// console.log(typeof idadeNumber)
+// console.log(typeof idadeString)
+
+// console.log(nome)
+// console.log(idade)
+
+// const jogador = "Henrique"
+// let gols = 3
+// let penalti = false 
+
+// console.log(typeof Bola)
+// console.log(typeof Gols)
+// console.log(typeof Penalti)
+
+// console.log("O Jogador",jogador, "Fez um total de",gols,"gols","mas nao sofreu penalti",penalti)
+
+
+let A = 30
+let B = 20
+
+let C; 
+console.log(A,B)
+
+C=A
+A=B
+B=C
+
+
+console.log(A,B)
+
+
